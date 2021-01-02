c5b11e74f151f7dba199e765a4549f374c3f7e54
Jackred <jackred@tuta.io>
2021-01-02 21:25:21 +0000

    ADD enum_map for card deck

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,40 @@
 use std::io;
 use rand::Rng;
+// use std::collections::HashMap;
 
+#[macro_use]
+extern crate enum_map;
+use enum_map::EnumMap;
+
+#[derive(Debug, Enum)]
 enum Resource {
     Wheat,
     Wood,
     Stone,
     Sheep,
     Clay,
-    None
 }
+
+#[derive(Debug, Enum)]
+enum ResearchCardValue {
+    Knight,
+    RoadBuilding,
+    YearOfPlenty,
+    Monopoly,
+    University,
+    Market,
+    GreatHall,
+    Chapel,
+    Library,
+}
+
+#[derive(Debug)]
+struct Deck {
+    resource_cards: EnumMap<Resource, i32>,
+    research_cards: EnumMap<Resource, i32>,
+}
+
+
 
 fn roll_dices(value: i32) -> (i32, i32) {
     (rand::thread_rng().gen_range(1, value),
@@ -28,6 +54,17 @@ fn main() {
     println!("Rolling the dices...");
     let (d1, d2) = roll_6_dices();
     println!("Dice 1: {}, Dice 2: {}", d1, d2);
-    
+    let map = enum_map! {
+        Resource::Wheat => 32,
+        _ => 0,
+    };
+    println!("{:?}", map[Resource::Stone]);
+    let deck = Deck {
+        resource_cards: map,
+        research_cards:  enum_map! {
+            _ => 0,
+        }
+    };
+    println!("{:?}", deck);
 }
 
