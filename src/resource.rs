a7104ce2453f73cfa9e12f884f56b7ff9d86d15e
Jackred <jackred@tuta.io>
2021-03-06 23:32:51 +0000

    ADD first try at add resource function
    need to be redone by using + operator
    newtype?

--- src/resource.rs
+++ src/resource.rs
@@ -10,3 +10,9 @@ pub enum Resource {
 }
 
 pub type ResourceDeck = EnumMap<Resource, i32>;
+
+pub fn add_resource(map: &mut ResourceDeck, to_add: ResourceDeck) {
+    for (key, value) in map {
+        *value += to_add[key]
+    }
+}
